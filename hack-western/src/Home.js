fe958bafa84ce3f06024cd421a0ed4465678d029
Kevin Chau <kchau57@uwo.ca>
2023-11-25 17:08:19 -0500

    hosting

--- hack-western/src/Home.js
+++ hack-western/src/Home.js
@@ -4,10 +4,12 @@ export default function Home() {
   return (
     <main>
     <div className="flex items-center justify-center h-screen">
-      <div className="flex flex-col gap-2">
-        <h1 className="text-4xl font-bold">Welcome to<br/>
-          <span className="text-8xl">Mocker.</span>
-        </h1>
+      <div className="flex flex-col gap-3">
+        <img 
+        src="logo.png"
+        alt="logo"
+        width="500px"
+        />
         <p>Helping students get closer to their dream internship</p>
         <div className="flex gap-5">
           <button className="bg-black text-white font-bold rounded p-3" onClick={()=> window.location.href='/ChatRoom'}>Start Now</button>
--- hack-western/src/server.js
+++ hack-western/src/server.js
@@ -25,5 +25,5 @@ io.on("connection", (socket) => {
 	})
 })
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => console.log(`server is running on port ${PORT}`))
