c9cc3d0d1a018f52dac8b5a429f1883f6b5f5d42
Kevin Chau <kchau57@uwo.ca>
2023-11-25 20:42:06 -0500

    mucking

--- hack-western/src/App.js
+++ hack-western/src/App.js
@@ -2,22 +2,10 @@ import Home from "./Home"
 import Main from "./Main"
 
 function App() {
-  const initialUserData = {
-    id: 1,
-    name: "John Doe",
-    experience: 0,
-    level: 1,
-    tasks: [],
-    tasksComplete: [],
-    mobsFarmed: []
-  };
-
-  console.log(initialUserData)
   return (
     <div>
       <Home />
       <Main />
-      console.log()
     </div>
   );
 }
--- hack-western/src/Main.js
+++ hack-western/src/Main.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
 import PomodoroTimer from './components/Pomodoro'
 
 export default function Main() {
+    const [userData, setUserData] = useState({
+        id: 1,
+        name: "John Doe",
+        experience: 0,
+        level: 1,
+        tasks: [],
+        tasksComplete: [],
+        mobsFarmed: [],
+      });
+    
+      const updateUserData = (updatedData) => {
+        setUserData(updatedData);
+      };
+      
+      console.log(userData);
+      
   return (
     <div className="h-screen flex justify-center items-center">
         <h1 className="font-bold text-9xl text-blue-500">HELLO WORLD!</h1>
-        <PomodoroTimer />
+        <PomodoroTimer userData={userData} updateUserData={updateUserData} />
     </div>
   )
 }
